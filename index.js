61f5fbeff058a18094eff0c4a40da29d7db9fe3e
Casey Van-Roose <caseyvanroose@Caseys-MacBook-Pro.local>
2018-08-31 14:29:39 +0100

    done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+
+const token = '5b27c0d09e4d5a042891a3c18dddd6350b057a2b'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
